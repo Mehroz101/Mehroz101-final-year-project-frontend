642e73867ce3b2973f37d445fd761c9eae29260f
Mehroz101 <125123032+Mehroz127@users.noreply.github.com>
2024-10-15 22:00:58 +0500

    check

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 const API_URL = import.meta.env.REACT_APP_API_URL;
-// const API_URL = "http://localhost:5000";
 
 export const signupUser = async (userData) => {
   // console.log("signupuser call");
